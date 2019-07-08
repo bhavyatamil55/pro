2027a42062a4933354f372afe8642a77e2919e0c
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-08 15:09:15 +0530

    Delete 10.py

--- 10.py
+++
@@ -1,8 +0,0 @@
-bhav1=int(input())
-bha2=[int(i) for i in input().split()]
-bhav3=0
-for i in range(bhav1):
-   for j in range(i):
-      if bhav2[j]<bhav2[i]:
-         bhav3+=bhav2[j]
-print(bhav3)        
