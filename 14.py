af11d3d5863466bb1dfc7ef45742e5a7392b2652
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-10 15:14:17 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,12 @@
+bhav,mah=map(int,input().split())
+list1=list(map(int,input().split()))
+bhav=[]
+list1.insert(0,0)
+for y in range(mah):
+     vin=[]
+     s=0
+     bb,zz=map(int,input().split())
+     for i in range(bb,zz+1):         
+         s^=list1[i]     
+     bhav.append(s)
+for y in bhav:
