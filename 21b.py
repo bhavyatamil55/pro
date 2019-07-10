3e24bf86128ccb061624c3303a4bf0939f172c71
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-10 17:16:16 +0530

    Delete 21b.py

--- 21b.py
+++
@@ -1,9 +0,0 @@
-test=int(input())
-arr=list(map(int,input().split()))
-avg=int(test/2)
-firavg=arr[:avg]
-secavg=arr[avg::]
-if ((sum(firavg)//len(firavg))==(sum(secavg)//len(secavg))):
-  print("yes")
-else:
-  print("no")
