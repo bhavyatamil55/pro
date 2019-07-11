741c90298e6177018c243163334a0ac4f18d95ab
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-11 11:51:30 +0530

    Update 36.py

--- 36.py
+++ 36.py
@@ -3,7 +3,7 @@ lack=list(map(int,input().split()))
 cas=0
 for i in range(0,len(lack)-2):
     for j in range(i+1,len(lack)-1):
-        for k in range(j+1,len(l)):
+        for k in range(j+1,len(lack)):
             if lack[i]>lack[j] and lack[j]>lack[k]:
                 cas+=1
 print(cas)
