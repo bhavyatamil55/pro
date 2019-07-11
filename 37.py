5e2df8e833439907502f40c2113867cf86b1f41a
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-11 11:55:03 +0530

    Update 37.py

--- 37.py
+++ 37.py
@@ -2,7 +2,7 @@ neet=int(input())
 liss=list(map(int,input().split()))
 cat=0
 for i in range(1,len(liss)-1):
-    if liss[i]>liss[i-1] and liss[i]>liss[i+1] or liss[i]<liss[i-1] and liss[i]<li[i+1]:
+    if liss[i]>liss[i-1] and liss[i]>liss[i+1] or liss[i]<liss[i-1] and liss[i]<liss[i+1]:
         cat+=1
 if len(liss)==1:
     print(1)
