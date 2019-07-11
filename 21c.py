7d160f8162fdd18c7aedf76082cf65ba48d17c80
bhavyatamil55 <51980999+bhavyatamil55@users.noreply.github.com>
2019-07-11 10:52:29 +0530

    Delete 21c.py

--- 21c.py
+++
@@ -1,9 +0,0 @@
-pink=int(input())
-ran=list(map(int,input().split()))
-ant=int(pink/2)
-laa=ran[:ant]
-lb=ran[ant::]
-if ((sum(laa)//len(laa))==(sum(lb)//len(lb))):
-    print("yes")
-else:
-    print("no")
